ba1b4014cc7d22c776e034473b60098749023c6f
humakasaat <hkasaat@hotmail.com>
2022-09-29 11:41:11 -0400

    first commit

---
+++ src/finallproject/FinallClass.java
@@ -0,0 +1,11 @@
+package finallproject;
+
+public class FinallClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println( " This is fianll project");
+
+	}
+
+}
